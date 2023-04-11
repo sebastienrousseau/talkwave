1028e9ddee2e6ef13a72de05c1b2921b4828b497
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2023-04-11 21:33:09 +0100

    test(talkwave): Increasing unit test coverage #4

---
+++ tests/test_curl.py
@@ -0,0 +1,237 @@
+import unittest
+from unittest import TestCase
+from unittest.mock import MagicMock, patch
+import json
+from requests.models import Response
+from datetime import datetime, timedelta
+from time import sleep
+from requests.exceptions import Timeout
+
+from curl import send_request
+
+
+class TestSendRequest(TestCase):
+
+    # test case for missing user_id
+    def test_missing_user_id(self):
+        api_key = 'test_api_key'
+        model = 'test_model'
+        prompt = 'test_prompt'
+        max_tokens = 10
+        temperature = 0.5
+        user_id = ''
+        rate_limit_seconds = 5
+        stop = None
+
+        with self.assertRaises(ValueError):
+            send_request(
+                api_key,
+                model,
+                prompt,
+                max_tokens,
+                temperature,
+                user_id,
+                rate_limit_seconds,
+                stop
+            )
+
+    # test case for rate limit period of 0
+    def test_rate_limit_period_of_0(self):
+        api_key = 'test_api_key'
+        model = 'test_model'
+        prompt = 'test_prompt'
+        max_tokens = 10
+        temperature = 0.5
+        user_id = 'test_user'
+        rate_limit_seconds = 0
+        stop = None
+
+        with self.assertRaises(ValueError):
+            send_request(
+                api_key,
+                model,
+                prompt,
+                max_tokens,
+                temperature,
+                user_id,
+                rate_limit_seconds,
+                stop
+            )
+
+    # test case for successful request
+    @patch('requests.post')
+    def test_successful_request(self, mock_post):
+        api_key = 'test_api_key'
+        model = 'test_model'
+        prompt = 'test_prompt'
+        max_tokens = 10
+        temperature = 0.5
+        user_id = 'test_user'
+        rate_limit_seconds = 5
+        stop = None
+
+        mock_response = MagicMock()
+        mock_response.status_code = 200
+        mock_response.json.return_value = {
+            'choices': [
+                {
+                    'text': 'test_text',
+                    'index': 0,
+                    'logprobs': None,
+                    'finish_reason': 'test_reason'
+                }
+            ]
+        }
+        mock_post.return_value = mock_response
+
+        response = send_request(
+            api_key,
+            model,
+            prompt,
+            max_tokens,
+            temperature,
+            user_id,
+            rate_limit_seconds,
+            stop
+        )
+
+        expected_response = {
+            'choices': [
+                {
+                    'text': 'test_text',
+                    'index': 0,
+                    'logprobs': None,
+                    'finish_reason': 'test_reason'
+                }
+            ]
+        }
+
+        self.assertEqual(response, expected_response)
+
+    # test case for timeout
+    @patch('requests.post')
+    def test_timeout(self, mock_post):
+        api_key = 'test_api_key'
+        model = 'test_model'
+        prompt = 'test_prompt'
+        max_tokens = 10
+        temperature = 0.5
+        user_id = 'test_user'
+        rate_limit_seconds = 5
+        stop = None
+
+        mock_post.side_effect = Timeout()
+
+        with self.assertRaises(Exception):
+            response = send_request(
+                api_key,
+                model,
+                prompt,
+                max_tokens,
+                temperature,
+                user_id,
+                rate_limit_seconds,
+                stop
+            )
+
+    # test case for stop
+    @patch('requests.post')
+    def test_stop(self, mock_post):
+        api_key = 'test_api_key'
+        model = 'test_model'
+        prompt = 'test_prompt'
+        max_tokens = 10
+        temperature = 0.5
+        user_id = 'test_user'
+        rate_limit_seconds = 5
+        stop = 'test_stop'
+
+        # First call to send_request should return a response without raising an exception
+        mock_response = MagicMock()
+        mock_response.status_code = 200
+        mock_response.json.return_value = {
+            'choices': [
+                {
+                    'text': 'test_text',
+                    'index': 0,
+                    'logprobs': None,
+                    'finish_reason': 'test_reason'
+                }
+            ]
+        }
+        mock_post.return_value = mock_response
+        response = send_request(
+            api_key,
+            model,
+            prompt,
+            max_tokens,
+            temperature,
+            user_id,
+            rate_limit_seconds,
+            stop
+        )
+        expected_response = {
+            'choices': [
+                {
+                    'text': 'test_text',
+                    'index': 0,
+                    'logprobs': None,
+                    'finish_reason': 'test_reason'
+                }
+            ]
+        }
+        self.assertEqual(response, expected_response)
+        self.assertEqual(mock_post.call_count, 1)
+
+    # test case for rate limit
+    @patch('requests.post')
+    def test_rate_limit(self, mock_post):
+        api_key = 'test_api_key'
+        model = 'test_model'
+        prompt = 'test_prompt'
+        max_tokens = 10
+        temperature = 0.5
+        user_id = 'test_user'
+        rate_limit_seconds = 5
+        stop = None
+
+        # First call to send_request should return a response without raising an exception
+        mock_response = MagicMock()
+        mock_response.status_code = 200
+        mock_response.json.return_value = {
+            'choices': [
+                {
+                    'text': 'test_text',
+                    'index': 0,
+                    'logprobs': None,
+                    'finish_reason': 'test_reason'
+                }
+            ]
+        }
+        mock_post.return_value = mock_response
+        response = send_request(
+            api_key,
+            model,
+            prompt,
+            max_tokens,
+            temperature,
+            user_id,
+            rate_limit_seconds,
+            stop
+        )
+        expected_response = {
+            'choices': [
+                {
+                    'text': 'test_text',
+                    'index': 0,
+                    'logprobs': None,
+                    'finish_reason': 'test_reason'
+                }
+            ]
+        }
+        self.assertEqual(response, expected_response)
+        self.assertEqual(mock_post.call_count, 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- tests/test_dir.py
+++ tests/test_dir.py
@@ -1,12 +1,35 @@
 import os
 import unittest
+from unittest import TestCase
 from dir import set_data_directory
 
 
-class TestDataDirectory(unittest.TestCase):
+class TestDataDirectory(TestCase):
 
+    # test case for setting the data directory
     def test_set_data_directory(self):
         path = "/tests/test_data"
+        expected_location = os.path.join(os.path.dirname(__file__), path)
+        actual_location = set_data_directory(path)
+        self.assertEqual(actual_location, expected_location)
+
+    # test case for setting the data directory to a non-existent directory
+    def test_set_data_directory_non_existent(self):
+        path = "/tests/non_existent"
+        expected_location = os.path.join(os.path.dirname(__file__), path)
+        actual_location = set_data_directory(path)
+        self.assertEqual(actual_location, expected_location)
+
+    # test case for setting the data directory to a file
+    def test_set_data_directory_file(self):
+        path = "/tests/test_data/test_file.txt"
+        expected_location = os.path.join(os.path.dirname(__file__), path)
+        actual_location = set_data_directory(path)
+        self.assertEqual(actual_location, expected_location)
+
+    # test case for setting the data directory to a directory
+    def test_set_data_directory_directory(self):
+        path = "/tests/test_data/test_directory"
         expected_location = os.path.join(os.path.dirname(__file__), path)
         actual_location = set_data_directory(path)
         self.assertEqual(actual_location, expected_location)
